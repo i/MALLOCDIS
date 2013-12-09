438d9f21466aec7260fec585471e82700dfefea1
Ian Lozinski <ian.lozinski@gmail.com>
2013-12-09 04:58:23 +0000

    Head only updating to last item in list

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@
 
 int main(int argc, char **argv) {
   char *test = my_malloc(10);
-  char *test2 = my_malloc(30);
-
   strcpy(test, "foo");
   printf("%s\n", test);
 
+  printList();
+/*  my_free(test);*/
+
+  char *test2 = my_malloc(30);
   strcpy(test2, "food");
   printf("%s\n", test2);
+
+/*  my_free(test2);*/
+
+  printList();
 
   return 0;
 }
--- src/malloc.h
+++ src/malloc.h
@@ -8,43 +8,54 @@ typedef struct block_ {
   unsigned int offset;
   unsigned int size;
   struct block_ *next;
+  struct block_ *prev;
   int free;
 } block;
 
 block *head = NULL;
 
+void destroy_node(block *n) {
+  free(n);
+}
 
 void *my_malloc(unsigned int size) {
   int i;
   block *p, *t;
-  /* If head is NULL, we create head */
+  /* This should happen the first time we malloc and create the block list */
   if (head == NULL) {
     head = malloc(sizeof(block));
     head->offset = 0;
     head->size = heap_size;
+    head->prev = NULL;
     head->next = NULL;
     head->free = 1;
   }
 
+  /* Iterate through to find a block that size fits into */
   for (p = head; p != NULL; p = p->next) {
-    if (p->free && p->size >= size) {
+    if (p->free && p->size >= size) {       /* Found suitable block */
       t = malloc(sizeof(block));
+      t->free = 0;
       t->size = size;
       t->offset = p->offset;
+      t->prev = p->prev;
       t->next = p;
-      t->free = 0;
-
+      p->prev = t;
       p->offset += size;
       p->size -= size;
-
-      printf("alloc'd %d bytes\n", size);
-      printf("0x%x\n", (int)(arry + t->offset));
       return arry + t->offset;
     }
   }
 
-  /* Return different error codes */
+  /* TODO Return different error codes */
   return NULL;
+}
+
+void printList() {
+  block *p;
+
+  for (p = head; p != NULL; p = p->next)
+    printf("OFFSET: %d, SIZE: %d, PHYSICAL ADDRESS: %p\n", p->offset, p->size, arry + p->offset);
 }
 /*
 [0,0,0,0,0]
@@ -67,5 +78,28 @@ void *my_malloc(unsigned int size) {
 [x, 0, y, 0, 0]
 */
 
+/* Frees a block enabling it to be allocated again */
 void my_free(void *foo) {
+  block *p, *t;
+  int offset = foo - (void *)arry;
+  int i = 0;
+  printf("Attempting to free from offset : %d\n", offset);
+
+  /* Check every block to see if it's the target */
+  for (p = head; p != NULL; p = p->next) {
+    printf("OFFSET: %d\n", p->offset);
+    if (p->offset == offset) {
+      printf("got here\n");
+      p->free = 1;                    /* Disable the lock on the block */
+      if (p->next && p->next->free) { /* If adjacent blocks are free, merge them! */
+        p->size += p->next->size;
+        t = p->next;
+        p->next = p->next->next;
+        destroy_node(t);              /* Clean up by destroying the node out of work */
+      }
+      printf("Freed at 0x%p\n", foo);
+      return;
+    }
+  }
+  /* TODO error code */
 }
